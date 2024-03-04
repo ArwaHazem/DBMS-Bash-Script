5124e0bf7685942b36dcdd2e21b5f9478e0132e2
ArwaHazem <arhazem93@gmail.com>
2024-03-04 22:45:05 +0200

    added table menu

---
+++ table-menu.sh
@@ -0,0 +1,38 @@
+#!/bin/bash
+
+echo "************************************Table Menu************************************"
+PS3="Enter Table Operation Number: "
+options=("Create Table" "Drop Table" "Insert into Table" "List Tables" "Select from Table" "Delete from Table" "Update Table" "Return To Databases Menu")
+select option in "${options[@]}";
+do
+    case $REPLY in
+        1) 
+            echo "Create Table"
+	    ;;
+        2) 
+            echo "Drop Table"
+            ;;
+        3) 
+            echo "Insert into Table" 
+            ;;
+        4) 
+            echo "List Tables"  
+            ;;
+        5) 
+            echo "Select from Table" 
+            ;;
+        6)
+            echo "Delete from Table"
+            ;;
+        7)
+            echo "Update Table"
+           ;;
+        8)
+	    echo "************************************Return To Databases Menu************************************"
+  	    cd .. #pwd after cd is supposed to be /DB
+            ./../db-menu.sh
+            break;;
+        *) 
+           echo "Invalid Input"
+    esac
+done
