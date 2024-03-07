cad0c4eff9b000c7802907223ec2ec52ae6e8c52
Huda22 <hudamohamed2223@gmail.com>
2024-03-07 10:54:26 +0200

    add : db-create script edit : db-list

--- db-start.sh
+++ db-start.sh
@@ -1,4 +1,15 @@
 #!/bin/bash
+
+#--------------- update scripts files premission -----------------------
+chmod +x db-start.sh
+chmod +x db-menu.sh
+chmod +x table-menu.sh
+chmod +x ./scripts/db-scripts/db-connect.sh
+chmod +x ./scripts/db-scripts/db-create.sh
+chmod +x ./scripts/db-scripts/db-drop.sh
+chmod +x ./scripts/db-scripts/db-list.sh
+#-----------------------------------------------------
+
 echo $PWD
 if [[ -d "./DB" ]]; then
     cd DB
--- scripts/db-scripts/db-list.sh
+++ scripts/db-scripts/db-list.sh
@@ -2,16 +2,20 @@
 export LC_COLLATE=C
 shopt -s extglob
 
-if [[ "$(basename "$PWD")" == "DB" ]]; 
-then
-     if [[ -z $(ls -d */ 2> /dev/null | cut -f1 -d'/' ) ]]
+function list_databases() {
+    if [[ "$(basename "$PWD")" == "DB" ]]; 
     then
-        echo -e "\nNo Database exist\n"
-    else
-        ls -d */ | cut -f1 -d'/'
-    fi
-    ./../db-menu.sh
+        if [[ -z "$(ls -d */ 2> /dev/null | cut -f1 -d'/' )" ]]
+            then
+                echo -e "\nNo Database exist\n"
+        else
+            ls -d */ | cut -f1 -d'/'
+        fi
     
 else 
     echo "Invalid Path"    
 fi
+
+}
+list_databases
+  
